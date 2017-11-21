1cd05bc455ac84f2b94dc4f62c8b0481f6f50113
John Hoskins <jbhoskins@email.wm.edu>
2017-11-21 12:04:51 -0500

    Removed binary file, updated gitignore, made slight change to (no yet ready) setup script.

--- src/setup.py
+++ src/setup.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name="RMTP Tagger",
       version="0.5",
-      description="Application for interactin with transcribing interviews\
-              in conjunction with an index of keywords that need to be \
-              tagged with specific xml tags.",
+      description=("Application for interactin with transcribing interviews "
+              "in conjunction with an index of keywords that need to be "
+              "tagged with specific xml tags."),
       author="John Hoskins and Margaret Swift",
       author_email="jbhoskins@email.wm.edu, meswift@email.wm.edu",
       url="https://github.com/jbhoskins/rmtpTagger",
       packages=["app", "app.gui", "app.backend"],
-      requires=["bs4"])
+      requires=["bs4"]
+#      entry_points={'console_scripts' : ['rmtp=app.gui.app.']
+      )
